f24ea9963393133f56eeaa081b929887b517f9c4
sinungzues <sinungzuestri@gmail.com>
2023-07-16 19:29:37 +0700

    Update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,67 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+// Toggle class active hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
 // Click diluar sidebar untuk menghilangkan navbar
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+
+// Modal Box
+const itemModalDetails =document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemModalDetails.style.display = 'flex';
+        e.preventDefault();
+    }
+}
+)
+
+
+// Klik Close button
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemModalDetails.style.display = 'none';
+    e.preventDefault();
+}
+
+// Click diluar modal
+window.onclick = (e) => {
+    if(e.target === itemModalDetails) {
+        itemModalDetails.style.display = 'none';
+    }
+};
